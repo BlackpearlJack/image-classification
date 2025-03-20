dc0a9c9f1f6f6ae556ad042e725f748d26d4e2c7
Lester Kevogo <lestercarlyle@gmail.com>
2025-03-20 14:17:16 +0300

    `Added image classification functionality and class number to name mapping in util.py`

--- server/util.py
+++ server/util.py
@@ -13,15 +13,28 @@ __model = None
 def classify_image(image_base64_data, file_path=None):
     imgs = get_cropped_image_if_2_eyes(file_path, image_base64_data)
     
+    result = []
+    
     for img in imgs:
         scalled_raw_img = cv2.resize(img, (32, 32))
         img_har = w2d(img,'db1',5)
         scalled_img_har = cv2.resize(img_har, (32, 32))
         combined_img = np.vstack((scalled_raw_img.reshape(32*32*3,1),scalled_img_har.reshape(32*32,1)))
         
-        len_image_array = 32 * 32 * 3 + 32 * 32
+        len_image_array = 32*32*3 + 32*32
         
-        final = combined_img(1,len_image_array).astype(float)
+        final = combined_img.reshape(1,len_image_array).astype(float)
+        
+        result.append({
+            'class': class_number_to_name(__model.predict(final)[0]),
+            'class_probability': np.round(__model.predict_proba(final)*100,2).tolist()[0],
+            'class_dictionary': __class_name_to_number
+        })
+    
+    return result
+
+def class_number_to_name(class_num):
+    return __class_number_to_name[class_num]
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
@@ -76,4 +89,8 @@ def get_b64_test_image_for_lovato():
         return f.read()
 
 if __name__ == '__main__':
+    load_saved_artifacts()
+    
     print(classify_image(get_b64_test_image_for_lovato(), None))
+    
+    # print(classify_image(None, ""))

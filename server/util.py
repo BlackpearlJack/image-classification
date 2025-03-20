cc6a71ba23da569d28768cfecb100f9f9d365bbb
Lester Kevogo <lestercarlyle@gmail.com>
2025-03-20 14:26:02 +0300

    `Added test image classification calls to util.py`

--- server/util.py
+++ server/util.py
@@ -93,4 +93,7 @@ if __name__ == '__main__':
     
     print(classify_image(get_b64_test_image_for_lovato(), None))
     
-    # print(classify_image(None, ""))
+    print(classify_image(None, "server/test-images/1c309ab9cb3d4841bc2820ec5045cdae.jpg"))
+    print(classify_image(None, "server/test-images/172e758f667241b98a1199c93c181f1b.jpg"))
+    print(classify_image(None, "server/test-images/4fb1d74bc5e60528d5e82d934894c69d.jpg"))
+    print(classify_image(None, "server/test-images/7e6b2009094eb8db7421da90b30bfa3c.jpg"))

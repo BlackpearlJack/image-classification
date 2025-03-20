5053349f20994473009c789c32f2598cc48450ab
Lester Kevogo <lestercarlyle@gmail.com>
2025-03-20 15:20:01 +0300

    `Added import from flask and updated route decorator`

--- server/server.py
+++ server/server.py
@@ -1,16 +1,17 @@
+from flask import Flask, request, jsonify
 import util
-from flask import Flask, request,jsonify 
 
 app = Flask(__name__)
 
-@app.route('/classify_image', methods = ['GET','POST'])
+
+@app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
     image_data = request.form['image_data']
-    
+
     response = jsonify(util.classify_image(image_data))
-    
+
     response.headers.add('Access-Control-Allow-Origin', '*')
-    
+
     return response
 
 if __name__ == "__main__":

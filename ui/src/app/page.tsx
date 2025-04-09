22d717d51e18e236e37946ad054ec80068fbed7e
Lester Kevogo <lestercarlyle@gmail.com>
2025-04-09 16:13:33 +0300

    `Added Navbar component and integrated it into the layout, also updated the Home page to display a list of players with images and names.`

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,13 +1,34 @@
+import Image from "next/image";
 import { AppButton } from "./components/common.components";
+// import chrisPic from "./0c02e3c0d732563b67ce27f6fe7f95ae.jpg";
+// import demiPic from "./1c309ab9cb3d4841bc2820ec5045cdae.jpg";
+
 
 export default function Home() {
+  const players = [
+    { name: "Chris Hemsworth", image:"", dataPlayer: "Chris_Hemsworth" },
+    { name: "Demi Lovato", image: "", dataPlayer: "Demi_Lovato" },
+    { name: "Ina Garten", image: "", dataPlayer: "Ina_Garten" },
+    { name: "Isabel Allende", image: "", dataPlayer: "Isabel_Allende"},
+    { name: "Lamine Yamal", image: "", dataPlayer: "Lamine_Yamal" },
+  ];
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <AppButton
-        title="Get Started"
-        link="/get-started"
-        className="bg-primaryColor-500 text-white hover:scale-3d"
-      />
+    <div className="container">
+      <div className="row">
+        {players.map((player, index) => (
+          <div key={index} className="col card-wrapper" data-player={player.dataPlayer}>
+            <div className="card border-0">
+              <div className="position-relative rounded-circle overflow-hidden mx-auto custom-circle-image">
+                {/* <Image src={player.image} alt={player.name} width={500} height={500}/> */}
+              </div>
+              <div className="card-body text-center mt-4">
+                <h5 className="text-uppercase card-title">{player.name}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

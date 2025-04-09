5766cb392df33a5ab76bd7409c91b98d8aa49d99
Lester Kevogo <lestercarlyle@gmail.com>
2025-04-09 15:12:30 +0300

    `Update Home component layout to use flex-col instead of flex`

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -2,7 +2,7 @@ import { AppButton } from "./components/common.components";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <AppButton
         title="Get Started"
         link="/get-started"

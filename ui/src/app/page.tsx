5b5acbe35458e0774b788dae4fce6bb58a126ab7
BlackpearlJack <lestercarlyle@gmail.com>
2025-04-10 08:25:10 +0300

    `Removed unused import and replaced AppButton with new layout`

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,13 +1,19 @@
-import { AppButton } from "./components/common.components";
+// import { AppButton } from "./components/common.components";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <AppButton
-        title="Get Started"
-        link="/get-started"
-        className="bg-primaryColor-500 text-white hover:scale-3d"
-      />
+    // <div className="flex items-center justify-center h-screen">
+    //   <AppButton
+    //     title="Get Started"
+    //     link="/get-started"
+    //     className="bg-primaryColor-500 text-white hover:scale-3d"
+    //   />
+    // </div>
+    <div className="container">
+      <div className="row">
+        <div className="col card-wrapper" data-player="lionel_messi">
+        </div>
+      </div>
     </div>
   );
 }

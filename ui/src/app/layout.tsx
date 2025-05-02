c2bd85b47d3d651e271478a1a83bf4a52b49ccbe
Lester Kevogo <lestercarlyle@gmail.com>
2025-05-02 12:23:28 +0300

    `Removed common.components.tsx file and updated import in layout.tsx to point to new location`

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Navbar } from "./components/common.components";
+import { Navbar } from "../components/common.components";
 
 export const metadata: Metadata = {
   title: "Create Next App",
